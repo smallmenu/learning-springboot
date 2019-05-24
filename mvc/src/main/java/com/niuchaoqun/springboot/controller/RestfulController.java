86afb2d3d1ee1c4ab81da2569b8c8d9b4946c4c5
niuchaoqun <smallmenu@gmail.com>
2019-05-24 15:53:57 +0800

    * 调整结构目录

--- mvc/src/main/java/com/niuchaoqun/springboot/controller/RestfulController.java
+++ mvc/src/main/java/com/niuchaoqun/springboot/controller/RestfulController.java
@@ -1,12 +1,18 @@
 package com.niuchaoqun.springboot.controller;
 
 import com.niuchaoqun.springboot.domain.User;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+import okhttp3.ResponseBody;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 import java.util.concurrent.atomic.AtomicLong;
 
 /**
@@ -37,6 +43,26 @@ public class RestfulController {
         user.setName(name);
 
         return user;
+    }
+
+    @RequestMapping("/url")
+    public String index(@RequestParam(value = "url") String url, HttpServletResponse response) {
+        OkHttpClient okhttpClient = new OkHttpClient().newBuilder().build();
+
+        try (Response res = okhttpClient.newCall(new Request.Builder()
+                .header("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36")
+                .url(url).build()).execute()) {
+            if (res.isSuccessful() && res.body() != null) {
+                ResponseBody body = res.body();
+                String string = body.string();
+                return string;
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return null;
+
     }
 
 

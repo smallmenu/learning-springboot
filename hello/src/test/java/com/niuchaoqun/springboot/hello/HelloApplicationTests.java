9f6e45569c791e596d25c1260914ba8b48792f3a
niuchaoqun <ncq@suosi.com.cn>
2020-03-16 11:49:13 +0800

    * update

---
+++ hello/src/test/java/com/niuchaoqun/springboot/hello/HelloApplicationTests.java
@@ -0,0 +1,11 @@
+package com.niuchaoqun.springboot.hello;
+
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class HelloApplicationTests {
+
+}

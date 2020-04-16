8f25f9c995e576b441d0324bfa0f4a751087597d
niuchaoqun <ncq@suosi.com.cn>
2020-04-16 19:38:42 +0800

    Default Changelist

--- springboot_security.sql
+++ springboot_security.sql
@@ -11,7 +11,7 @@
  Target Server Version : 50729
  File Encoding         : 65001
 
- Date: 10/04/2020 19:34:22
+ Date: 16/04/2020 19:38:04
 */
 
 SET NAMES utf8mb4;
@@ -42,10 +42,10 @@ CREATE TABLE `admin`  (
 -- ----------------------------
 -- Records of admin
 -- ----------------------------
-INSERT INTO `admin` VALUES (1, 'windspider', '管理员', '$2a$10$7xspkfCj3yOuHlE6bhLavuFr5xVmDhxxhwR7DGtHLWoKerjTHhuS6', 'windspider@suosi.com.cn', '127.0.0.1', '2020-04-10 05:33:37', 0, 1, '2018-12-12 10:36:25', 1, '2020-04-10 05:33:37', 1);
+INSERT INTO `admin` VALUES (1, 'windspider', '管理员', '$2a$10$7xspkfCj3yOuHlE6bhLavuFr5xVmDhxxhwR7DGtHLWoKerjTHhuS6', 'windspider@suosi.com.cn', '127.0.0.1', '2020-04-16 08:40:44', 0, 1, '2018-12-12 10:36:25', 1, '2020-04-16 08:40:43', 1);
 INSERT INTO `admin` VALUES (9, 'niuchaoqun', 'fasdfasdf', '$2a$10$jsr6xoYHPLmfxZNdVlKrneAIMG7KyFQCv2b8rk5sG7br0gNRz3WoW', 'asdf@asdf.asdf', '', NULL, 0, 1, '2020-04-10 05:36:54', 1, '2020-04-10 05:36:54', 0);
 INSERT INTO `admin` VALUES (10, 'niuchaoqun1', 'fasdfasdf', '$2a$10$SrAgjwD0mP6Y4HEQqQU5pOia1.dtNrg9ClAnBGjhzGg5UWCwd8i0i', 'asdf@asdf.asdf', '', NULL, 0, 1, '2020-04-10 09:35:40', 1, '2020-04-10 09:35:40', 0);
-INSERT INTO `admin` VALUES (11, 'niuchaoqun3', 'fasdfasdf', '$2a$10$OB8Ahd4gTT..pWX2j2VE3OSg1nVzp8dOjhWWz2bwDfHRT8QegXxFy', 'asdf@asdf.asdf', '', NULL, 0, 1, '2020-04-10 09:36:22', 1, '2020-04-10 09:36:22', 0);
+INSERT INTO `admin` VALUES (11, 'niuchaoqun3', 'fasdfasdf', '$2a$10$OB8Ahd4gTT..pWX2j2VE3OSg1nVzp8dOjhWWz2bwDfHRT8QegXxFy', 'asdf@asdf.asdf', '', NULL, 0, 1, '2020-04-10 09:36:22', 1, '2020-04-16 08:42:49', 1);
 
 -- ----------------------------
 -- Table structure for admin_login_log
@@ -59,7 +59,12 @@ CREATE TABLE `admin_login_log`  (
   `logined` timestamp(0) NOT NULL DEFAULT CURRENT_TIMESTAMP(0) COMMENT '登录时间',
   `state` tinyint(1) NOT NULL DEFAULT 0 COMMENT '是否成功',
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci COMMENT = '登录日志' ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 2 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci COMMENT = '登录日志' ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of admin_login_log
+-- ----------------------------
+INSERT INTO `admin_login_log` VALUES (1, 1, 'windspider', '127.0.0.1', '2020-04-16 08:40:43', 1);
 
 -- ----------------------------
 -- Table structure for admin_role
@@ -70,7 +75,7 @@ CREATE TABLE `admin_role`  (
   `adminid` int(10) UNSIGNED NOT NULL,
   `roleid` int(10) UNSIGNED NOT NULL,
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 6 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 13 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of admin_role
@@ -78,17 +83,16 @@ CREATE TABLE `admin_role`  (
 INSERT INTO `admin_role` VALUES (1, 1, 1);
 INSERT INTO `admin_role` VALUES (2, 9, 2);
 INSERT INTO `admin_role` VALUES (3, 10, 2);
-INSERT INTO `admin_role` VALUES (4, 11, 2);
-INSERT INTO `admin_role` VALUES (5, 11, 3);
+INSERT INTO `admin_role` VALUES (11, 11, 1);
+INSERT INTO `admin_role` VALUES (12, 11, 3);
 
 -- ----------------------------
 -- Table structure for role
 -- ----------------------------
 DROP TABLE IF EXISTS `role`;
 CREATE TABLE `role`  (
-  `id` int(10) UNSIGNED NOT NULL DEFAULT 0 AUTO_INCREMENT COMMENT 'ID',
+  `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT COMMENT 'ID',
   `name` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '角色名称',
-  `count` int(10) UNSIGNED NOT NULL DEFAULT 0 COMMENT '管理员数量',
   PRIMARY KEY (`id`) USING BTREE,
   UNIQUE INDEX `name`(`name`) USING BTREE
 ) ENGINE = InnoDB AUTO_INCREMENT = 4 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci COMMENT = '角色' ROW_FORMAT = Dynamic;
@@ -96,9 +100,9 @@ CREATE TABLE `role`  (
 -- ----------------------------
 -- Records of role
 -- ----------------------------
-INSERT INTO `role` VALUES (1, '管理员', 1);
-INSERT INTO `role` VALUES (2, '运营', 3);
-INSERT INTO `role` VALUES (3, '推广', 1);
+INSERT INTO `role` VALUES (3, '推广');
+INSERT INTO `role` VALUES (1, '管理员');
+INSERT INTO `role` VALUES (2, '运营');
 
 -- ----------------------------
 -- Table structure for setting
